777cace0b236fcae79a6096907bb4167758cd26b
Lester Li <Lester.shiyi.li@gmail.com>
2018-04-12 07:29:04 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+<div class="container">
+    <p class="pull-left">
+      <a href="">Lester</a> Coding
+    </p>
+    <p class="pull-left"> <a href="mailto:lester.shiyi.li@gmail.com"></a> Contact us</p>
+</div>
+</footer>
---
+++ resources/views/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Index page')
+
+@section('content')
+  <h1>This is the index page</h1>
+@stop
