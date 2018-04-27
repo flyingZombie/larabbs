ceb85c69bf7d9e442730f658dde6420cdd44e22a
Lester Li <lester.shiyi.li@gmail.com>
2018-04-27 20:23:43 +1000

    Users can edit their own topic

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -32,9 +32,9 @@
 
                 <div class="form-group">
                     <select class="form-control" name="category_id" required>
-                      <option value="" hidden disabled selected>Please choose category</option>
+                      <option value="" hidden disabled {{ $topic->id ? '': 'selected' }}>Please choose category</option>
                       @foreach ($categories as $value)
-                        <option value="{{ $value->id }}">{{ $value->name }}</option>
+                        <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
                       @endforeach
                     </select>
                 </div>
