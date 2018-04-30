d82c1bea3f862b2e3e871f28f4e927c838e4474f
Lester Li <Lester.shiyi.li@gmail.com>
2018-04-30 05:13:26 +0000

    Notify author when topic replied

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -30,7 +30,7 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
-                <li><a href="{{ route('login') }}">SignIn</a></li>
+                <li><a href="{{ route('login') }}">Sign In</a></li>
                 <li><a href="{{ route('register')}}">Register</a></li>
                 @else
                 <li>
@@ -38,6 +38,16 @@
                         <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                     </a>
                 </li>
+
+                {{-- Notifications--}}
+                <li>
+                  <a href="{{ route('notifications.index') }}" class="notifications-badge" style="margin-top: -2px;">
+                    <span class="badge badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'fade'}}" title="消息提醒">
+                        {{ Auth::user()->notification_count }}
+                    </span>
+                  </a>
+                </li>
+
                 <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
---
+++ resources/views/notifications/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('title')
+My notices
+@stop
+
+@section('content')
+
+    <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                    <h3 class="text-center">
+                        <span class="glyphicon glyphicon-bell" aria-hidden="true">
+                        </span>
+                        My notices
+                    </h3>
+                    <hr>
+
+                    @if ($notifications->count())
+                        <div class="notification-list">
+                            @foreach($notifications as $notification)
+                                @include('notifications.types._'. snake_case(class_basename($notification->type)))
+                            @endforeach
+
+                            {!! $notifications->render() !!}
+                        </div>
+
+                    @else
+
+                        <div class="empty-block">No notice!</div>
+
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+@stop
