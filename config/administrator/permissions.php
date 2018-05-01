f074138e5f73a78b6fc48e2b30856fb2f65d2b55
Lester Li <Lester.shiyi.li@gmail.com>
2018-05-01 10:05:13 +0000

    Admin can manage content

--- config/administrator/permissions.php
+++ config/administrator/permissions.php
@@ -63,4 +63,17 @@ return [
             'title' => '标示',
         ],
     ],
+
+    'menu' => [
+        '用户与权限' => [
+            'users',
+            'roles',
+            'permissions',
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
+    ],
 ];
