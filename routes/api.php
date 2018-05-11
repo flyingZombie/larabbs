a95773a3860ed28e1c61a194799bbfe8c9ff4e70
Lester Li <Lester.shiyi.li@gmail.com>
2018-05-11 06:54:33 +0000

    topics store

--- routes/api.php
+++ routes/api.php
@@ -58,9 +58,12 @@ $api->version('v1', [
                 // 当前登陆用户信息
                 $api->get('user', 'UsersController@me')
                     ->name('api.user.show');
-                //图片资源
+                // 图片资源
                 $api->post('images', 'ImagesController@store')
                     ->name('api.images.store');
+                // 发布话题
+                $api->post('topics', 'TopicsController@store')
+                    ->name('api.topics.store');
                 //编辑登录用户信息
                 $api->patch('user', 'UsersController@update')
                     ->name('api.user.update');
