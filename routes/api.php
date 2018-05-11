a9b7fc2f194c1319a7f256cf76184c9ffd21a309
Lester Li <Lester.shiyi.li@gmail.com>
2018-05-11 02:29:26 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,8 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
     ], function ($api) {
 
         $api->group([
@@ -49,5 +50,11 @@ $api->version('v1', [
             // 删除token
             $api->delete('authorizations/current', 'AuthorizationsController@destroy')
                 ->name('api.authorizations.destroy');
+            // 需要token验证的接口
+            $api->group(['middleware' => 'api.auth'], function ($api) {
+                // 当前登陆用户信息
+                $api->get('user', 'UsersController@me')
+                    ->name('api.user.show');
+            });
         });
     });
